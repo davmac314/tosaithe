2b729221f7d484c808b3d1d683bb5bdc5365186f
Davin McCall <davmac@davmac.org>
2021-09-13 21:31:04 +1000

    Read menu entries from tosaithe.conf config file

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,11 @@
-#include <stddef.h>
+#include <vector>
+#include <type_traits>
+#include <memory>
+#include <new>
+#include <string>
+#include <string_view>
+
+#include <cstddef>
 
 #include "uefi.h"
 #include "uefi-loadedimage.h"
@@ -11,96 +18,102 @@
 EFI_BOOT_SERVICES *EBS;
 EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
 
-// Load an entire file, in a block of allocated pages
-void *load_entire_file(EFI_DEVICE_PATH_PROTOCOL *devPath, UINTN *bufSize)
+class load_file_exception
 {
-    EFI_HANDLE loadDevice;
-    EFI_STATUS status = EBS->LocateDevicePath(&EFI_simple_file_system_protocol_guid, &devPath, &loadDevice);
+public:
+    enum lf_stage {
+        NO_FSPROTOCOL_FOR_DEV_PATH,
+        CANNOT_OPEN_VOLUME,
+        NO_DPTT_PROTOCOL, // firmware lacks DEVICE_PATH_TO_TEXT
+        CANNOT_OPEN_FILE,
+        CANNOT_GET_FILE_SIZE,
+        CANNOT_READ_FILE
+    };
+
+    lf_stage reason;
+    EFI_STATUS status = 0;
+
+    load_file_exception(lf_stage reason_p) : reason(reason_p) {}
+    load_file_exception(lf_stage reason_p, EFI_STATUS status_p)
+        : reason(reason_p), status(status_p) {}
+};
+
+// Load an entire file, in pool-allocated memory
+//   throws: load_file_exception, std::bad_alloc
+void *load_entire_file(EFI_DEVICE_PATH_PROTOCOL *dev_path, UINTN *buf_size_ptr)
+{
+    EFI_HANDLE load_dev_hndl;
+    EFI_STATUS status = EBS->LocateDevicePath(&EFI_simple_file_system_protocol_guid, &dev_path,
+            &load_dev_hndl);
     if (EFI_ERROR(status)) {
-        ConOut->OutputString(ConOut, L"Couldn't get file system protocol for file's device path\r\n");
-        return nullptr;
+        throw load_file_exception(load_file_exception::NO_FSPROTOCOL_FOR_DEV_PATH, status);
     }
 
-    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *sfsProtocol = nullptr;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *sfs_protocol = nullptr;
 
-    status = EBS->HandleProtocol(loadDevice, &EFI_simple_file_system_protocol_guid, (void **)&sfsProtocol);
-    if (EFI_ERROR(status)) {
-        ConOut->OutputString(ConOut, L"Couldn't get file system protocol for file path\r\n");
-        return nullptr;
-    }
-    if (sfsProtocol == nullptr) {
-        ConOut->OutputString(ConOut, L"Couldn't get file system protocol for file path (firmware misbehaving)\r\n");
-        return nullptr;
+    status = EBS->HandleProtocol(load_dev_hndl, &EFI_simple_file_system_protocol_guid,
+            (void **)&sfs_protocol);
+    if (EFI_ERROR(status) || sfs_protocol == nullptr) {
+        // This shouldn't happen; firmware is misbehaving?
+        throw load_file_exception(load_file_exception::NO_FSPROTOCOL_FOR_DEV_PATH, status);
     }
 
     EFI_FILE_PROTOCOL *fsRoot = nullptr;
-    status = sfsProtocol->OpenVolume(sfsProtocol, &fsRoot);
-    if (EFI_ERROR(status)) {
-        ConOut->OutputString(ConOut, L"Couldn't open volume (fs protocol)\r\n");
-        return nullptr;
-    }
-    if (fsRoot == nullptr) {
-        ConOut->OutputString(ConOut, L"Couldn't open volume (fs protocol); firmware misbehaving\r\n");
-        return nullptr;
+    status = sfs_protocol->OpenVolume(sfs_protocol, &fsRoot);
+    if (EFI_ERROR(status) || fsRoot == nullptr) {
+        throw load_file_exception(load_file_exception::CANNOT_OPEN_VOLUME, status);
     }
 
     // Need to convert remaining path to string path (within filesystem)
     EFI_DEVICE_PATH_TO_TEXT_PROTOCOL *dpToTextProto =
-            (EFI_DEVICE_PATH_TO_TEXT_PROTOCOL *)locateProtocol(EFI_device_path_to_text_protocol_guid);
+            (EFI_DEVICE_PATH_TO_TEXT_PROTOCOL *)locate_protocol(EFI_device_path_to_text_protocol_guid);
 
     if (dpToTextProto == nullptr) {
-        con_write(L"Firmware doesn't support DEVICE_PATH_TO_TEXT protocol\r\n");
         // TODO combine the path ourselves
         fsRoot->Close(fsRoot);
-        return nullptr;
+        throw load_file_exception(load_file_exception::NO_DPTT_PROTOCOL);
     }
 
-    CHAR16 *filePath = dpToTextProto->ConvertDevicePathToText(devPath,
-            false /* displayOnly */, false /* allowShortcuts */);
+    EFI_FILE_PROTOCOL *file_to_load = nullptr;
 
-    EFI_FILE_PROTOCOL *fileToLoad = nullptr;
-    status = fsRoot->Open(fsRoot, &fileToLoad, filePath, EFI_FILE_MODE_READ, 0);
-    if (EFI_ERROR(status) || fileToLoad == nullptr) {
-        ConOut->OutputString(ConOut, L"Couldn't open file\r\n");
-        freePool(filePath);
+    {
+        CHAR16 *file_path = dpToTextProto->ConvertDevicePathToText(dev_path,
+                false /* displayOnly */, false /* allowShortcuts */);
+
+        status = fsRoot->Open(fsRoot, &file_to_load, file_path, EFI_FILE_MODE_READ, 0);
+
+        free_pool(file_path);
         fsRoot->Close(fsRoot);
-        return nullptr;
+
+        if (EFI_ERROR(status) || file_to_load == nullptr) {
+            throw load_file_exception(load_file_exception::CANNOT_OPEN_FILE, status);
+        }
     }
 
-    fsRoot->Close(fsRoot);
-
-    EFI_FILE_INFO *loadFileInfo = get_file_info(fileToLoad);
-    if (loadFileInfo == nullptr) {
-        ConOut->OutputString(ConOut, L"Couldn't get file size\r\n");
+    EFI_FILE_INFO *load_file_info = get_file_info(file_to_load);
+    if (load_file_info == nullptr) {
+        file_to_load->Close(file_to_load);
+        throw load_file_exception(load_file_exception::CANNOT_GET_FILE_SIZE, status);
     }
 
-    UINTN readAmount = loadFileInfo->FileSize;
-    free_pool(loadFileInfo);
+    UINTN read_amount = load_file_info->FileSize;
+    free_pool(load_file_info);
 
-    void *loadAddress = alloc(readAmount);
-    if (loadAddress == nullptr) {
-        con_write(L"Couldn't allocate memory to load file");
-        fileToLoad->Close(fileToLoad);
-        return nullptr;
+    void *load_address = alloc_pool(read_amount);
+    if (load_address == nullptr) {
+        file_to_load->Close(file_to_load);
+        throw std::bad_alloc();
     }
 
-    status = fileToLoad->Read(fileToLoad, &readAmount, (void *)loadAddress);
-    fileToLoad->Close(fileToLoad);
+    status = file_to_load->Read(file_to_load, &read_amount, load_address);
+    file_to_load->Close(file_to_load);
     if (EFI_ERROR(status)) {
-        ConOut->OutputString(ConOut, L"Couldn't read file; ");
-        CHAR16 errcode[3];
-        errcode[2] = 0;
-        errcode[1] = hexdigit(status & 0xFu);
-        errcode[0] = hexdigit((status >> 4) & 0xFu);
-        ConOut->OutputString(ConOut, L"EFI status: 0x");
-        ConOut->OutputString(ConOut, errcode);
-        ConOut->OutputString(ConOut, L"\r\n");
-        freePool(loadAddress);
-        return nullptr;
+        free_pool(load_address);
+        throw load_file_exception(load_file_exception::CANNOT_READ_FILE, status);
     }
 
-    if (bufSize) *bufSize = readAmount;
-    return (void *)loadAddress;
+    if (buf_size_ptr) *buf_size_ptr = read_amount;
+    return load_address;
 }
 
 static EFI_DEVICE_PATH_PROTOCOL *resolve_relative_path(EFI_HANDLE image_handle, const CHAR16 *path)
@@ -160,6 +173,229 @@ static EFI_STATUS chain_load(EFI_HANDLE ImageHandle, const CHAR16 *ExecPath, con
 
 EFI_STATUS load_stivale2(EFI_HANDLE ImageHandle, const CHAR16 *exec_path, const CHAR16 *cmdLine);
 
+struct menu_entry {
+    enum entry_type_t {
+        CHAIN,
+        LINUX_CHAIN,
+        STIVALE2
+    };
+
+    std::wstring description;
+    entry_type_t entry_type = CHAIN;
+    std::wstring exec_path;
+    std::wstring cmdline;
+
+    menu_entry() { }
+
+    menu_entry(const CHAR16 *description_p, entry_type_t entry_type_p, const CHAR16 *exec_path_p,
+            const CHAR16 *cmdline_p)
+        : description(description_p), entry_type(entry_type_p), exec_path(exec_path_p), cmdline(cmdline_p) { }
+};
+
+void skip_ws(std::string_view &sv)
+{
+    while (sv.length() > 0 && (sv[0] == ' ' || sv[0] == '\t' || sv[0] == '\r' || sv[0] == '\n')) {
+        sv.remove_prefix(1);
+    }
+}
+
+void skip_to_next_line(std::string_view &sv)
+{
+    while (!sv.empty() && sv[0] != '\r' && sv[0] != '\n') {
+        sv.remove_prefix(1);
+    }
+
+    if (sv.empty()) return;
+
+    while (!sv.empty() && (sv[0] == '\r' || sv[0] == '\n')) {
+        sv.remove_prefix(1);
+    }
+}
+
+bool is_ident_lead(char c)
+{
+    return c >= 'a' && c <= 'z';
+}
+
+bool is_ident(char c)
+{
+    return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_';
+}
+
+std::string_view read_ident(std::string_view &sv)
+{
+    const char *start = sv.data();
+    while (!sv.empty() && is_ident(sv[0])) sv.remove_prefix(1);
+    return std::string_view(start, sv.data() - start);
+}
+
+class parse_exception : public std::exception
+{
+    const char *what_msg;
+public:
+    parse_exception(const char *msg) noexcept : what_msg(msg) { }
+    const char *what() const noexcept override { return what_msg; }
+};
+
+static const char * const msg_colon_after_entry = "expecting ':' after 'entry'";
+static const char * const msg_lbrace_after_entry = "expecting '{' after 'entry:'";
+static const char * const msg_rbrace_after_entry = "expecting '}' at end of entry";
+static const char * const msg_equals_after_var = "expecting '=' after identifier in entry setting";
+static const char * const msg_value_after_equals = "expecting value after '=' in entry setting";
+
+class utf8to16
+{
+    std::wstring output;
+
+public:
+    void process(char c)
+    {
+        int i = c;
+        if ((i & 0x80u) == 0) {
+            // plain ascii
+            output += (wchar_t)i;
+        } else {
+            // TODO
+        }
+    }
+
+    void process(const char *s)
+    {
+        while (*s != 0) {
+            process(*s);
+            s++;
+        }
+    }
+
+    std::wstring &get_output()
+    {
+        return output;
+    }
+};
+
+
+std::wstring read_assignment_value(std::string_view &conf)
+{
+    skip_ws(conf);
+    if (conf.empty() || conf[0] != '=') throw parse_exception {msg_equals_after_var};
+    conf.remove_prefix(1);
+    skip_ws(conf);
+    if (conf.empty()) throw parse_exception {msg_value_after_equals};
+
+    if (is_ident_lead(conf[0])) {
+        std::string_view value = read_ident(conf);
+        // TODO check for trailing junk (allow trailing comment)
+        skip_to_next_line(conf);
+
+        utf8to16 uu;
+        for (char c : value) {
+            uu.process(c);
+        }
+
+        return uu.get_output();
+    }
+    else if (conf[0] == '\'') {
+        conf.remove_prefix(1);
+        utf8to16 uu;
+        while (!conf.empty() && conf[0] != '\'') {
+            uu.process(conf[0]);
+            conf.remove_prefix(1);
+        }
+
+        // TODO check that we got trailing '''
+        // TODO check for trailing junk (allow trailing comment)
+        skip_to_next_line(conf);
+
+        return uu.get_output();
+    }
+    else {
+        // XXX
+    }
+
+    return L""; // XXX
+}
+
+// parse an entry - everything between braces
+menu_entry parse_entry(std::string_view &conf)
+{
+    menu_entry entry;
+
+    while (!conf.empty() && conf[0] != '}') {
+        if (conf[0] == '#') {
+            skip_to_next_line(conf);
+        }
+        else if (is_ident_lead(conf[0])) {
+            std::string_view ident = read_ident(conf);
+            std::wstring value = read_assignment_value(conf);
+            if (ident == "description") {
+                entry.description = std::move(value);
+            }
+            else if (ident == "exec") {
+                entry.exec_path = std::move(value);
+            }
+            else if (ident == "type") {
+                if (value == L"chain") {
+                    entry.entry_type = menu_entry::CHAIN;
+                }
+                else if (value == L"linux_chain") {
+                    entry.entry_type = menu_entry::LINUX_CHAIN;
+                }
+                else if (value == L"stivale2") {
+                    entry.entry_type = menu_entry::STIVALE2;
+                }
+                else {
+                    // XXX
+                }
+            }
+            else if (ident == "cmdline") {
+                entry.cmdline = std::move(value);
+            }
+        }
+        else {
+            return entry;
+        }
+
+        skip_ws(conf);
+    }
+
+    return entry;
+}
+
+std::vector<menu_entry> parse_config(char *conf_buf, UINTN buf_size)
+{
+    std::vector<menu_entry> result;
+    std::string_view conf {conf_buf, buf_size};
+    std::string_view sv_entry = "entry";
+
+    skip_ws(conf);
+
+    while (!conf.empty()) {
+        if (conf[0] == '#') {
+            skip_to_next_line(conf);
+        }
+        else if (is_ident_lead(conf[0])) {
+            std::string_view ident = read_ident(conf);
+            if (ident == sv_entry) {
+                skip_ws(conf);
+                if (conf.empty() || conf[0] != ':') throw parse_exception {msg_colon_after_entry};
+                conf.remove_prefix(1); skip_ws(conf);
+                if (conf.empty() || conf[0] != '{') throw parse_exception {msg_lbrace_after_entry};
+                conf.remove_prefix(1); skip_ws(conf);
+                result.push_back(parse_entry(conf));
+                if (conf.empty() || conf[0] != '}') throw parse_exception {msg_rbrace_after_entry};
+                conf.remove_prefix(1);
+            }
+            else {
+                // XXX
+            }
+        }
+
+        skip_ws(conf);
+    }
+
+    return result;
+}
+
 extern "C"
 EFI_STATUS
 EFIAPI
@@ -173,18 +409,75 @@ EfiMain (
 
     ConOut->ClearScreen(ConOut);
 
-    con_write(L"Stigorge boot menu\r\n");
+    con_write(L"Tosaithe boot menu\r\n");
     con_write(L"Firmware vendor: ");
     con_write(SystemTable->FirmwareVendor);
     con_write(L"\r\n\r\n");
+
+    auto conf_path = efi_unique_ptr_wrap(resolve_relative_path(ImageHandle, L"\\tosaithe.conf"));
+    if (conf_path == nullptr) {
+        // An error message should already be out, but let's make it clear why we will abort now:
+        con_write(L"Could not load '\\tosaithe.conf'.\r\n");
+        return EFI_LOAD_ERROR;
+    }
+
+    std::vector<menu_entry> menu;
+
+    efi_unique_ptr<char> conf_buf;
+    UINTN conf_size;
+
+    try {
+        conf_buf = efi_unique_ptr_wrap((char *) load_entire_file(conf_path.get(), &conf_size));
+    }
+    catch (load_file_exception &lfe) {
+        con_write(L"Could not load 'tosaithe.conf'");
+        if (lfe.reason == load_file_exception::CANNOT_OPEN_FILE) {
+            con_write(L" - can't open; ");
+            if (lfe.status == EFI_NOT_FOUND) {
+                con_write(L" file not found.");
+            }
+            else {
+                con_write(L" error ");
+                con_write(lfe.status);
+                con_write(L".");
+            }
+        }
+        else {
+            con_write(L" - system error.");
+        }
+        con_write(L"\r\n");
+        return EFI_LOAD_ERROR;
+    }
+
+    conf_path.reset();
+
+    try {
+        menu = parse_config(conf_buf.get(), conf_size);
+    }
+    catch (parse_exception &pe) {
+        utf8to16 uu;
+        uu.process(pe.what());
+
+        con_write(L"Error in tosaithe.conf: ");
+        con_write(uu.get_output().c_str());
+
+        return EFI_LOAD_ERROR;
+    }
+
+    conf_buf.reset();
 
     ConOut->SetAttribute(ConOut, EFI_YELLOW);
     con_write(L"Please make a selection:\r\n\r\n");
     ConOut->SetAttribute(ConOut, EFI_LIGHTCYAN);
 
-    con_write(L"1. Linux - vmlinuz-5.10.47-pstore root=/dev/nvme0n1p2\r\n");
-    con_write(L"2. EFI shell\r\n");
-    con_write(L"3. Badux\r\n");
+    unsigned i = 1;
+    for (const auto &entry : menu) {
+        con_write(i);
+        con_write(L". ");
+        con_write(entry.description.c_str());
+        con_write(L"\r\n");
+        i++;
+    }
 
     ConOut->SetAttribute(ConOut, EFI_LIGHTGRAY);
     con_write(L"\r\n=>");
@@ -207,11 +500,21 @@ EfiMain (
     con_write(keyStr);
     ConOut->SetAttribute(ConOut, EFI_LIGHTGRAY);
 
-    if (keyPr.UnicodeChar == L'1') {
-        return chainLoad(ImageHandle, L"\\vmlinuz-5.10.47-pstore", L"linux root=/dev/nvme0n1p2");
-    } else if (keyPr.UnicodeChar == L'2') {
-        return chainLoad(ImageHandle, L"\\EFI\\Shell.efi", L"Shell.efi");
-    } if (keyPr.UnicodeChar == L'3') {
+    if (keyPr.UnicodeChar < L'1' || keyPr.UnicodeChar > L'9') {
+        return EFI_LOAD_ERROR; // XXX
+    }
+
+    unsigned index = keyPr.UnicodeChar - L'1';
+    if (index >= menu.size()) {
+        return EFI_LOAD_ERROR; // XXX
+    }
+    const menu_entry &entry = menu[index];
+
+    if (entry.entry_type == menu_entry::CHAIN) {
+        return chain_load(ImageHandle, entry.exec_path.c_str(), entry.cmdline.c_str());
+    } else if (entry.entry_type == menu_entry::LINUX_CHAIN) {
+        return chain_load(ImageHandle, entry.exec_path.c_str(), entry.cmdline.c_str());
+    } else {
         return load_stivale2(ImageHandle, L"\\badux.elf", L"");
     }
 
