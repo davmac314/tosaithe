40c52d3f6e6a654db7f8d43780b8abb847b02e05
Davin McCall <davmac@davmac.org>
2021-09-17 22:29:37 +1000

    Add scripts to clone required libraries and build them

---
+++ clone-libs.sh
@@ -0,0 +1,6 @@
+# Clone the dependencies here, so they can be built with the correct
+# build flags, and automatically linked into tosaithe build.
+
+git clone git@github.com:davmac314/bmcxxabi.git bmcxxabi
+git clone git@github.com:davmac314/bmunwind.git bmunwind
+git clone git@github.com:davmac314/libbmcxx.git libbmcxx
---
+++ rebuild-libs.sh
@@ -0,0 +1,9 @@
+# Rebuild the dependencies.
+
+rm -f lib/libbmcxx.a lib/libcxxabi.a lib/libunwind.a
+(cd bmcxxabi; make clean)
+(cd libbmcxx; make clean)
+(cd bmunwind; make clean)
+make lib/libcxxabi.a
+make lib/libbmcxx.a
+make lib/libunwind.a
