3cd988748ca58bb715c18330c04857de5eb5f0a7
Davin McCall <davmac@davmac.org>
2021-09-20 22:17:48 +1000

    Create lib directory before attempting to build libs into it

--- rebuild-libs.sh
+++ rebuild-libs.sh
@@ -4,6 +4,7 @@ rm -f lib/libbmcxx.a lib/libcxxabi.a lib/libunwind.a
 (cd bmcxxabi; make clean)
 (cd libbmcxx; make clean)
 (cd bmunwind; make clean)
+mkdir lib
 make lib/libcxxabi.a
 make lib/libbmcxx.a
 make lib/libunwind.a
