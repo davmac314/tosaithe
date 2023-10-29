6fb403503e9ece3afca66011f6b1c7c9c2e01869
Davin McCall <davmac@davmac.org>
2023-10-29 10:25:48 +1000

    build: check out known-good commits for each dependency

--- clone-libs.sh
+++ clone-libs.sh
@@ -10,3 +10,7 @@ else
     git clone git@github.com:davmac314/bmunwind.git bmunwind
     git clone git@github.com:davmac314/libbmcxx.git libbmcxx
 fi
+
+git -c advice.detachedHead=false -C bmcxxabi checkout 834aff7495f4bae9d3b839eb6659485cf5683751
+git -c advice.detachedHead=false -C bmunwind checkout 4ee7583c10b6993e2c65955cba2e3ed2d32e829e
+git -c advice.detachedHead=false -C libbmcxx checkout 65c65c257eda35a5966daa1d3efd37eb6401f8bb
