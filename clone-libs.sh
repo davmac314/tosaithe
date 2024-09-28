6bffcc9f9f1d8cd7f93981da95cb010a3060b93c
Davin McCall <davmac@davmac.org>
2024-09-28 23:08:38 +1000

    Update to latest bmcxxabi

--- clone-libs.sh
+++ clone-libs.sh
@@ -11,6 +11,6 @@ else
     git clone git@github.com:davmac314/libbmcxx.git libbmcxx
 fi
 
-git -c advice.detachedHead=false -C bmcxxabi checkout eb504acd75249e42dacb5e9117f823d5454a08d6
+git -c advice.detachedHead=false -C bmcxxabi checkout a01e3750224d375e919c8990edf8704943e2c4ba
 git -c advice.detachedHead=false -C bmunwind checkout 3a10d7046d1e3da920cc67b058f7625b6871ad52
 git -c advice.detachedHead=false -C libbmcxx checkout f35b540d91378c43017fba91ccb93e6277901939
